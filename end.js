3619207dc6b4ee70d7470cb2f2881f90b2a74729
Austin Perez <austin.perez760@gmail.com>
2021-01-07 01:27:58 -0800

    updated end js local storage inputs now save and post within an array, user can now input there scores and be routed to main screen once done

--- end.js
+++ end.js
@@ -1,13 +1,34 @@
 const nickname = document.getElementById("nickname");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
 const userFinalScore = document.getElementById("userFinalScore");
 const recentScore = localStorage.getItem("recentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const MAX_HIGH_SCORES = 5;
+console.log(highScores);
 
 userFinalScore.innerText = recentScore;
 
 nickname.addEventListener("keyup", () => {
-  console.log("usernameInput.value");
+  saveScoreBtn.disabled = !nickname.value;
 });
 
-function saveFinalScore() {
-  console.log("clicked");
-}
+saveHighScore = (e) => {
+  console.log("clicked button");
+  e.preventDefault();
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    nickname: nickname.value,
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => {
+    return b.score - a.score;
+  });
+  highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("index.html");
+
+  console.log(highScores);
+};
